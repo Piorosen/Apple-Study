31dbaf48d1891f3b46612260f916d28f55edc0fb
Piorosen <aoikazto@gmail.com>
2020-02-08 05:47:09 +0900

    toolbar 을 만들었다..!

--- Swift/MacOS/R-Studio/R-Studio/AppDelegate.swift
+++ Swift/MacOS/R-Studio/R-Studio/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
+    @IBOutlet weak var toolbar: NSToolbar!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
@@ -27,6 +28,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.center()
         window.setFrameAutosaveName("Main Window")
         window.contentView = NSHostingView(rootView: contentView)
+        
+        window.toolbar = toolbar
         window.makeKeyAndOrderFront(nil)
     }
 
