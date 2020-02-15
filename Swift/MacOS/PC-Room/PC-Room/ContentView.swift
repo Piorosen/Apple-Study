735e44b8df7b887e056e974d0db5a32efc126a76
Piorosen <aoikazto@gmail.com>
2020-02-15 23:53:31 +0900

    pc room first commit

---
+++ Swift/MacOS/PC-Room/PC-Room/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PC-Room
+//
+//  Created by Aoikazto on 2020/02/15.
+//  Copyright © 2020 Aoikazto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
